5e8d2c5a0c60018cad5ae7207ff2fa19421d05fe
hugeblank <hugeblank@gmail.com>
2024-12-04 16:19:44 -0800

    feat: rest of the owl

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,26 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<svelte:head>
+	<title>it's hugeblank</title>
+	<meta name="description" content="the homepage">
+		<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://hugeblank.dev"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:title" content="it's hugeblank"/>
+	<meta property="og:description" content="the home page"/>
+	<meta property="og:image" content="https://hugeblank.dev/images/hugeblank.png"/>
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image"/>
+	<meta property="twitter:domain" content="hugeblank.dev"/>
+	<meta property="twitter:url" content="https://hugeblank.dev"/>
+	<meta name="twitter:title" content="it's hugeblank"/>
+	<meta name="twitter:description" content="the home page"/>
+	<meta name="twitter:image" content="https://hugeblank.dev/images/hugeblank.png"/>
+</svelte:head>
+
+<div class="min-w-7xl max-w-7xl pr-8 pl-8 ml-auto mr-auto">
+	{@render children()}
+	<footer class="text-center mt-24">hugeblank is a registered trademark of Hugeblank Entertainment. For more information please make a blood sacrifice to the harvest goddess Demeter.</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col text-center place-content-center">
+    <div class="flex flex-row place-content-center gap-3">
+        <img class="w-10 h-10 self-center" src="favicon.png" alt="A bagel">
+        <h1>Welcome to </h1>
+        <h1 title="you are here">hugeblank.dev</h1>
+        <img class="w-10 h-10 self-center" src="favicon.png" alt="A bagel">
+    </div>
+    <p>Important Viewing:</p>
+    <video controls class="min-w-1/6 ml-auto mr-auto">
+        <source src="nene~yayboom.mp4" type="video/mp4"/>
+        <track src="nene~yayboom.vtt" kind="captions">
+    </video>
+    <h2 class="pt-4">Hello</h2>
+    <p class="w-3/5 mr-auto ml-auto">
+        I'm hugeblank, or Todd depending on the time of day and who you ask. 
+        I have a bachelors degree in CS and use my skills in ways that would make my professors wish they didn't let me graduate.
+        I spend most of my time programming in Java, but got my degree using other languages like C/C++, Python, and frameworks like Flutter and Django.
+        Recently I've been dipping my toe into web development via typescript, and most recently with Svelte.
+        I'd like to get a job so if you're hiring shoot me an email or something.
+    </p>
+    <p class="w-3/5 mr-auto ml-auto">
+        Over the last few years I've also gained an unhealthy obsession over public transit (trains), <a class="text-cyan-600 hover:underline" href="https://baub.au">vTubers</a>, and Japanese culture, and even the language itself. 
+        This obsession has (naturally) resulted in me learning Japanese and going on a 2 month trip to Japan, where I visited over 20 cities and made over <a id="oops" href="#oops" title="I forgot one already" class="line-through">20</a> 19 memories.
+        I will definitely be making a blog post about that so please stay tuned.
+    </p>
+    <h2 class="pt-4">I have a blog</h2>
+    <p><a href="https://blog.hugeblank.dev/" class="text-cyan-600 hover:underline">it is here</a>.</p>
+    <h2 class="pt-4">I have projects</h2>
+    <p>Like this Minecraft mod that embeds a Lua interpreter to allow you to write Lua mods. <a href="https://github.com/Moonflower-Mods/allium" class="text-cyan-600 hover:underline">it is here</a>.</p>
+    <p>Or this other mod that fixes Minecraft's daylight cycle skipping when on a laggy server. <a href="https://modrinth.com/mod/asahi" class="text-cyan-600 hover:underline">it is here</a>.</p>
+    <p>Or my blog which is actually a custom frontend for <a href="https://whtwnd.com" class="text-cyan-600 hover:underline">WhiteWind</a>, a blog service that utilizes <a href="https://atproto.com" class="text-cyan-600 hover:underline">ATProto</a>. <a href="https://github.com/hugeblank/whitebreeze" class="text-cyan-600 hover:underline">it is WhiteBreeze</a>.</p>
+    <h2 class="pt-4">I have social media</h2>
+    <p>My bluesky is <a href="https://bsky.hugeblank.dev/" class="text-cyan-600 hover:underline">@hugeblank.dev</a>.</p>
+    <p>My signal is <a id="nowhere" href="#nowhere" title="Sorry this one goes nowhere.">@hugeblank.01</a>.</p>
+    <p>My discord is <a href="#nowhere" title="Sorry this one also goes nowhere.">@hugeblank</a>.</p>
+    <p>My email is <a href="mailto:hugeblank@gmail.com" class="text-cyan-600 hover:underline">hugeblank@gmail.com</a>.</p>
+    <p>My steam is <a href="https://steamcommunity.com/id/hugeblank/" class="text-cyan-600 hover:underline">hugeblank</a>.</p>
+    <h2 class="pt-4 ">The badges</h2>
+    <div class="mr-auto ml-auto grid grid-cols-3 gap-y-2 pt-2">
+        <a href="https://aspenr.dev"><img src="images/aspenr.dev.gif" alt="aspenr.dev badge" style="margin-left: 8px;"></a>
+        <a href="https://ally.cat"><img src="images/ally.cat.png" alt="ally.cat badge" style="margin-left: 8px;"></a>
+        <a href="https://danii.fi"><img src="images/danii.fi.gif" alt="danii.fi badge" style="margin-left: 8px;"></a>
+        <a href="https://github.com/tmpim"><img src="images/tmpim.pw.png" alt="github.com/ badge" style="margin-left: 8px;"></a>
+        <a href="https://kotahu.com"><img src="images/kotahu.com.gif" alt="kotahu.com badge" style="margin-left: 8px;"></a>
+        <a href="https://archlinux.org/"><img src="images/archlinux.org.gif" alt="archlinux.org badge" style="margin-left: 8px;"></a>
+        <!-- Other badges go above --> 
+        <p>My badge:</p><img src="images/hugeblank.dev.gif" alt="The official hugeblank.dev badge" style="margin-left: 8px;">
+    </div>
+    <h2 class="pt-4">Bagel lore</h2>
+    <h3 class="pt-2">The name</h3>
+    <p class="w-3/5 mr-auto ml-auto">
+        The name "hugeblank" dates back to 2010 when I was in my local library, signing up for Roblox. I had a few criteria for the name.
+        I didn't want it to have any numbers, and I wanted it to be comprised of real words. That made things difficult enough, 
+        but to top it all off Roblox was already really popular back then, despite it being entirely different from what it is now.
+        The only saving grace for me was that below the username box, if you put in a username that already existed,
+        it would provide a few alternatives that weren't already taken. I spent what felt like an eternity typing combinations of words in with no luck whatsoever.
+        It got to the point that I gave up and just slammed the keyboard and got a bunch of gibberish which was, hilariously, taken.
+        But whatever keysmash I threw in opened the username suggestion area's third eye. Up until now it had just been putting numbers onto what I wrote in, but
+        this time, it came close to the answer. hugeblank0. I took that username, put it in the input box, and then removed the 0 and prayed. 
+        It worked, I signed up, and the rest is history.
+    </p>
+    <h3 class="pt-2">The bagel</h3>
+    <p class="w-3/5 mr-auto ml-auto">
+        The bagel motif was a later occurence in the hugeblank lore. It happened due to a user in a Minecraft server I was playing on back in 2014. 
+        They were messing with anagrams of other online players usernames and eventually got to me. They landed on "bagel hunk" which I thought was funny.
+        At that time I didn't exactly have a profile icon I was set on using, so I went and googled a photo of a bagel, and found this one:
+    </p>
+    <img class="min-w-1/6 ml-auto mr-auto" src="images/hugeblank.png" alt="Watercolor Rendering of a plain bagel by Judy Unger" title="Watercolor Rendering of a plain bagel by Judy Unger"/>
+    <p class="w-3/5 mr-auto ml-auto">
+        While working on this website I decided to finally see if I could find the source, and with relative ease I was able to.
+        Please enjoy Judy Unger's blog, <a href="https://foodartist.wordpress.com/2011/02/23/41-i-had-a-hole-lox-of-fun-illustrating-bagels/">Illustrating My Life #41</a>
+        It surprised me to find out that what I had been using this whole time was an illustration, and not a photo. Her work is incredibly impressive and when I have the free time
+        I will be spending it reading through and admiring her images.
+    </p>
+</div>
